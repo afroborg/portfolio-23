800015e65cc0fc794e22792b73d88340015eb5b8
Axel Froborg <axel.froborg@gmail.com>
2023-03-05 13:59:35 +0100

    add graident background

---
+++ src/components/feature/start/gradient-bg.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  // export let prop: string = '';
+</script>
+
+<div />
+
+<style lang="scss">
+  div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: radial-gradient(
+      circle,
+      theme('colors.primary.yellow / 5%') 0%,
+      theme('colors.primary.yellow / 0%') 100%
+    );
+  }
+</style>
